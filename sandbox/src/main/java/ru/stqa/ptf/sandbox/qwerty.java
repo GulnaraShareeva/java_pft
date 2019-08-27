cf19945d416e8c40586028b461e426dc25780bb9
GulnaraShareeva <shareeva.gulnara@yandex.ru>
2019-08-27 19:24:11 +0300

    настройки

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/qwerty.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/qwerty.java
@@ -1,9 +1,9 @@
 package ru.stqa.ptf.sandbox;
 
 public class qwerty {
-	
-public static void main(String[] args) {
-	System.out.println("Hello, world!");
-  }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
