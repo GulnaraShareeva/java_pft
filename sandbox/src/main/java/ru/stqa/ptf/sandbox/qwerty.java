78873fb3de911e052909c27d0c6cfbe680e62089
GulnaraShareeva <shareeva.gulnara@yandex.ru>
2019-08-30 16:43:57 +0300

    добавлена функция для точек

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/qwerty.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/qwerty.java
@@ -6,24 +6,29 @@ public class qwerty {
         hello ("world");
         hello ("user");
         hello ("Gulnara");
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area (l));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area ());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        double x1 = 1;
+        double x2 = 2;
+        double y1 = 3;
+        double y2 = 4;
+        System.out.println("Расстояние между точками  = " + distance(x1, x2, y1, y2));
+    }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello," + somebody+ "!");
+    }
+
+    public static double distance (double x1, double x2, double y1, double y2 ) {
+        return Math.sqrt((y2 - y1) *  (y2 - y1) + (x2 - x1) *  (x2 - x1));
 
     }
-        public static void hello (String somebody) {
-            System.out.println("Hello," + somebody+ "!");
-        }
-        public static double area(double len) {
-       return len * len;
-        }
-
-         public static double area(double a, double b) {
-        return a*b;
-         }
 
 
 }
+
